2a76a82d729a8916cbce47328565fcc324966f74
MouliBhaskaran <moulibhaskaran@gmail.com>
2023-02-27 19:53:43 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import AuthContext from "./Context/AuthContext";
+import Navigation from "./Components/Navigation";
 
-function App() {
+const App = () => {
+  const [auth, setAuth] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <AuthContext.Provider
+            value={{
+              isLoggedIn: auth,
+              setLoggedIn: (value) => {
+                setAuth(value);
+              },
+            }}
+          >
+            <Navigation />
+          </AuthContext.Provider>
+        </header>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
